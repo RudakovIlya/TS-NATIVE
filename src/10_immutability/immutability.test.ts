127184d10e90fee7b5359ac9c7e0b9b7d69c11bb
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2022-12-07 16:39:54 +0300

    feat: watched a lecture about immutability

--- src/10_immutability/immutability.test.ts
+++ src/10_immutability/immutability.test.ts
@@ -1,9 +1,9 @@
 import {
-    addBookForNewUser,
+    addBookForNewUser, addNewCompanies,
     isAge,
-    moveToMoscow,
+    moveToMoscow, updateBooks, updateUserCompaniesName,
     upgradeLaptopForNewUser,
-    UserType, UserWithBooks,
+    UserType, UserWithBooks, UserWithCompanies,
     UserWithLaptop
 } from "./immutability";
 
@@ -97,4 +97,66 @@ test('copy books', () => {
     expect(user.address.title).toBe('Minsk');
     expect(user.laptop).toBe(userCopy.laptop);
     expect(user.books).not.toBe(userCopy.books);
+    expect(userCopy.books[3]).toBe('Redux');
+    expect(user.books[3]).toBe(undefined);
+    expect(user.books.length).toBe(3);
+    expect(userCopy.books.length).toBe(4);
+})
+
+test('update js to ts', () => {
+    let user: UserWithLaptop & UserWithBooks = {
+        name: ' Dimych',
+        hair: 32,
+        age: 32,
+        address: {
+            title: 'Minsk'
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: ['Js', 'React', 'css', 'html'],
+    }
+
+    const newUserBooks = updateBooks(user, 'Js', 'TS');
+
+    expect(user).not.toBe(newUserBooks);
+    expect(user.books).not.toBe(newUserBooks.books);
+    expect(user.books.length).toBe(4);
+    expect(newUserBooks.books.length).toBe(4);
+    expect(newUserBooks.books[0]).toBe('TS');
+    expect(user.books[0]).toBe('Js');
+
+    expect(user).not.toBe(newUserBooks);
+    expect(user.address).toBe(newUserBooks.address);
+    expect(newUserBooks.address.title).toBe('Minsk');
+    expect(user.address.title).toBe('Minsk');
+    expect(user.laptop).toBe(newUserBooks.laptop);
+
+})
+
+test('test companies', () => {
+    const user: UserWithLaptop & UserWithCompanies = {
+        name: ' Dimych',
+        hair: 32,
+        age: 32,
+        address: {
+            title: 'Minsk'
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        companies: [{id: 1, title: 'EPAM'}, {id: 2, title: 'IN-INCUBATOR'}]
+    }
+
+    const copyUserWithNewCompanies = addNewCompanies(user);
+
+    const newUser = updateUserCompaniesName(user, 'Епам', 1)
+
+    expect(user).not.toBe(copyUserWithNewCompanies);
+    expect(user.companies).not.toBe(copyUserWithNewCompanies.companies);
+
+    expect(user.companies).not.toBe(newUser.companies);
+    expect(user.companies[0].title).toBe('EPAM')
+    expect(newUser.companies[0].title).toBe('Епам')
+
 })
--- src/10_immutability/immutability.ts
+++ src/10_immutability/immutability.ts
@@ -15,6 +15,15 @@ export type UserWithBooks = UserType & {
     books: Array<string>
 }
 
+type CompanyType = {
+    id: number
+    title: string
+}
+
+export type UserWithCompanies = {
+    companies: CompanyType[]
+}
+
 export function isAge(u: UserType) {
     const copy = {
         ...u,
@@ -31,6 +40,20 @@ export const upgradeLaptopForNewUser = (user: UserWithLaptop, laptop: string) =>
     return {...user, laptop: {...user.laptop, title: laptop}}
 }
 
-export const addBookForNewUser = (user: UserWithLaptop & UserWithBooks, book: string) => {
-    return {...user, books: [...user.books, book]}
-}
+export const addBookForNewUser = (user: UserWithLaptop & UserWithBooks, book: string) => ({
+    ...user,
+    books: [...user.books, book]
+})
+export const updateBooks = (user: UserWithLaptop & UserWithBooks, oldBook: string, newBook: string) => ({
+    ...user, books: user.books.map(item =>
+        item === oldBook ? newBook : item
+    )
+})
+
+export const addNewCompanies = (users: UserWithLaptop & UserWithCompanies) => (
+    {...users, companies: [...users.companies, {id: 3, title: 'Google'}]}
+)
+
+export const updateUserCompaniesName = (users: UserWithLaptop & UserWithCompanies, title: string, id: number) => (
+    {...users, companies: users.companies.map(c => c.id === id ? {...c, title} : c)}
+)
